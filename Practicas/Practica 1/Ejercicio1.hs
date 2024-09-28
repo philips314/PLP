b6e8731c5a8fef829823a455150b47d651e9fe3d
Philips <142275614+philips314@users.noreply.github.com>
2024-09-28 16:53:03 -0300

    Update Ejercicio1.hs

--- Practicas/Practica 1/Ejercicio1.hs
+++ Practicas/Practica 1/Ejercicio1.hs
@@ -3,11 +3,27 @@ max2 (x, y)
      | x >= y = x
      | otherwise = y
 
+normaVectorial (x,y) :: (Float, Float) -> Float
+normaVectorial (x, y) = sqrt (x^2 + y^2)
 
-- normaVectorial (x, y) = sqrt (x^2 + y^2)
-- subtract = flip (-)
-- predecesor = subtract 1
-- evaluarEnCero = \f -> f 0
-- dosVeces = \f -> f . f
-- flipAll = map flip
+subtract :: Float -> Float -> Float
+subtract = flip (-)
+
+predecesor :: Float -> Float
+predecesor = subtract 1
+
+evaluarEnCero :: (Float -> a) -> b
+evaluarEnCero = \f -> f 0
+
+dosVeces :: (a -> a) -> a -> a
+dosVeces = \f -> f . f 
+-- f . f = f (f a) = f a = a
+
+flipAll :: [a -> b -> c] -> [b -> a -> c]
+flipAll = map flip
+-- flip :: (a -> b -> c) -> (b -> a -> c)
+-- map :: (a -> b) -> [a] -> [b]
+-- El a del map queda (a -> b -> c) y el b queda (b -> a -> c)
+--Intuitivamente, espera una lista de funciones a las que se les aplica flip (provocando que estas esperen sus argumentos en orden inverso)
+
 - flipRaro = flip flip
